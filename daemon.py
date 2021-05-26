cc7e592e6797ab530bcfaf087d295e94463d194d
shin donghyuck <shin_donghyuc@cygames.co.jp>
2021-05-26 21:43:38 +0900

    ----- 위치변경

--- daemon.py
+++ daemon.py
@@ -69,11 +69,12 @@ ETH/JPY : {eth_info['price']}({eth_info['delta']})
 가격변동률 : rate({getDeltaRate(eth_info['price'] , eth_info['last_price'])}%)
 ASK : {eth_info['ask']}
 BID : {eth_info['bid']}
----------------------------
+
 BTC/JPY : {btc_info['price']}({btc_info['delta']}) 
 가격변동률 : rate({getDeltaRate(btc_info['price'] , btc_info['last_price'])}%)
 ASK : {btc_info['ask']}
 BID : {btc_info['bid']}
+---------------------------
         """
 
         postSlack(post_url, text)
